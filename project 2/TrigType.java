2a163329d54a5efc168e9126a97ace2848f01859
Leon <leonlam34@gmail.com>
2022-03-21 00:12:46 -0500

    -

--- project 2/TrigType.java
+++ project 2/TrigType.java
@@ -1,5 +1,21 @@
-public class TrigType {
+
+
+enum TrigType {
+
+    COSINE("COSINE"), SIN("SIN");
+
+    private final String type;
+
+    private TrigType(String type){
+        this.type = type;
+    }
+    public String getType(){return type;}
+}
+
+
+//TODO: make sure that this eumeration class works with everything else 
+class TrigSwitch{
     public static void main(String[] args) {
-        System.out.println(1);    
+        TrigType t;
     }
 }
