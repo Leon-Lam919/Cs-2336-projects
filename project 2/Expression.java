b33f055d9c391c730b6ad9c7df5273eecc9e8458
poke213 <leonlam34@gmail.com>
2022-03-22 18:13:48 -0500

    -

--- project 2/Expression.java
+++ project 2/Expression.java
@@ -3,13 +3,14 @@ class Expression{
     //constructor method 
     public Expression(){}
 
-    //getter
+    // getter taht returns a new expression object containing the derivate of each term of the original obj
     public Expression getDerivative(){
         // created instance of expression and returns expression
         Expression E = new Expression();
         return E;
     }
 
+    // TODO: get the evaluation of the expression that is sent in
     // gets eval
     public double getEvaluation(double expression){
         // returns double
@@ -18,14 +19,20 @@ class Expression{
     }
 
     public String toString(){
-        // return string
+        // TODO: returns the input to string type
         return null;
     }
 
     public void add(ITerm x){
 
-        // creates and adds a linked list of ITerm objs
+        // TODO: add ITerm objects to the linked list
         
     }
+
+    //TODO: create a ProjLinkedList of ITerms
+
+    ProjLinkedList<ITerm> LL = new ProjLinkedList<>();
+
+    
 
 }
--- project 2/ProjLinkedList.java
+++ project 2/ProjLinkedList.java
@@ -1,4 +1,4 @@
-public class ProjLinkedList {
+public class ProjLinkedList<T> {
 // TODO: figure out if the linked list works and make sure all the fucntions work within it
 // TODO: currrently does not work with other classes and cannot be called from main
     
