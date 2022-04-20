f075f23736a1106fae6f233dde32419f2d9eec63
Leon <leonlam34@gmail.com>
2022-04-20 12:35:13 -0500

    -

--- project 3/Main.java
+++ project 3/Main.java
@@ -4,20 +4,26 @@ public class Main{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        // asking for table size
         System.out.print("Enter table size: ");
         int table = in.nextInt();
 
+        // asking for initial hash val
         System.out.print("Enter initial hash value: ");
         int hashVal = in.nextInt();
 
+        // asking for hash multiplier
         System.out.print("Enter hash multiplier: ");
         int hashMulti = in.nextInt();
 
+        // asking for prime val
         System.out.print("Enter relative prime value: ");
         int primeVal = in.nextInt();
 
+        // creating an instance of the StringHash obj, passing all the above values to it
         StringHash SH = new StringHash(table, hashVal, hashMulti, primeVal);
 
+        // menu with while loop to keeping running while usr doesn't choose "Q"
         String quit = "";
         while (quit != "Q"){
         System.out.println("1. Search String");
