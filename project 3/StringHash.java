ba3b27dea341751255814ed901d7bc672fe89407
Leon <leonlam34@gmail.com>
2022-04-19 20:57:39 -0500

    -

---
+++ project 3/StringHash.java
@@ -0,0 +1,66 @@
+import java.lang.String;
+
+public class StringHash {
+    private int size, initialvalue, hashMultiplier, relativePrime;
+
+    // size setter
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setInitialValue(int initialvalue){
+        this.initialvalue = initialvalue;
+    }
+
+    public void setHashMultiplier(int hashMultiplier){
+        this.hashMultiplier = hashMultiplier;
+    }
+
+    public void setRelativePrime(int relativePrime){
+        this.relativePrime = relativePrime;
+    }
+    
+    public int getSize(){
+        return size;
+    }
+
+    public int getInitialvalue() {
+        return initialvalue;
+    }
+
+    public int getHashMultiplier() {
+        return hashMultiplier;
+    }
+    
+    public int getRelativePrime() {
+        return relativePrime;
+    }
+
+    // constructor StringHash method
+    public StringHash(int size, int initialvalue, int hashMultiplier, int relativePrime){
+        System.out.println("hi");
+    }
+
+    // method that sees if the StringHash contains the value
+    boolean contains(String data){
+        return false;
+    }
+
+    // method that adds the value to the StringHash
+    boolean add(String data){
+        return false;
+    }
+
+    
+    boolean remove(String data){
+        return false;
+    }
+
+    void displayTable(){
+
+    }
+
+    void resize(){
+
+    }
+}
