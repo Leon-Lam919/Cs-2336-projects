04a031b80cda7ebe6d8e9545190457226a2249ac
poke213 <leonlam34@gmail.com>
2022-11-28 11:47:49 -0600

    -

--- ex.java
+++ ex.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 
 
-public class ex {
-    public static void main(String[] args) {
-    
-        Scanner sc = new Scanner(System.in);
-        System.out.print("how many dogs do you have: ");
-        String r = sc.nextLine();
 
-        System.out.println(r);
-        
-        
-        
-        sc.close();
+public class ex {
+
+        //function
+        public static int turrn(){
+            return 0;
+        }
+
+    public static void main(String[] args) {
+        System.out.println("0");
+        turrn();
     }
+
+
 }
